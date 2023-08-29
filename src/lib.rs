c7bb03827faa0d7f5910f1ccb56bc78ce5e7f517
wesley-lewis <lewis.wes.23@gmail.com>
2023-08-29 21:51:08 +0530

    Used iterators and closures

--- src/lib.rs
+++ src/lib.rs
@@ -28,13 +28,9 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line); 
-        }
-    }
-    results
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a> (query: &str, contents: &'a str) -> Vec<&'a str> {
@@ -50,17 +46,22 @@ pub fn search_case_insensitive<'a> (query: &str, contents: &'a str) -> Vec<&'a s
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() > 2 {
-            let query = args[1].clone();
-            let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-            let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-            Ok(Config { query, filename, case_sensitive })
-        } else {
-            // panic!("Usage --query --filename");
-            return Err("Not enough arguments");
-        }       
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(filename) => filename,
+            None => return Err("Didn't get a filename"),
+        };
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config{ query, filename, case_sensitive })
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,8 @@ extern crate minigrep;
 use minigrep::{ Config, run};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();    
 
-    let config: Config = Config::new(&args).unwrap_or_else(|err| {
+    let config: Config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
